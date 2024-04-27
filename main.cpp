77693b40cfa2e6a6fee6945aa631ad7a9db923e0
Peter Pham <sythanhpham@gmail.com>
2024-04-27 00:34:35 -0400

    Adding RPLidar

---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include <csignal>
+#include "RPLidar.hpp"
+
+bool running=true; //Variable to manage stops when signals are received
+void signal_handler(int signo){
+	if(signo==SIGTERM || signo==SIGINT)
+		running = false;
+}
+
+int main(int argc, char** argv){
+	/* ************************************
+	 *      SETUP LIDAR & CHECK STATUS    *
+	 **************************************/
+	signal(SIGTERM, signal_handler);
+	signal(SIGINT, signal_handler);
+	RPLidar lidar; //Connects to lidar
+	running = lidar.init(argc>1?argv[argc - 1]:"/dev/ttyUSB0");
+	/* ************************************
+	 *               START 			      *
+	 **************************************/
+	if(running) {
+		lidar.print_status();
+		running = lidar.start();
+	}
+
+	while(running){
+		//Update current scan (one turn of measurements)
+		lidar.update();
+		lidar.print_scan();
+		lidar.print_deltas();
+	}
+	/* ***********************************
+	 *              STOP ALL             *
+	 *************************************/
+	return 0;
+}
